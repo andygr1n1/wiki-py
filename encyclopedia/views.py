db038e78754f409b5d872e8b244d7aec5a075d10
Grini Andrei <andrei.grini@carasent.com>
2024-09-10 07:58:22 +0300

    feat: Search

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,9 +1,9 @@
 import logging
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
 from . import util
 
-# logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
@@ -12,8 +12,19 @@ def index(request):
 
 def entry(request, title):
     content = util.get_entry(title)
-    # logging.info(f"Content for title '{title}': {content}")
+
     if content:
         return render(request, 'encyclopedia/entry.html', {'content': content, 'title': title})
     else:
         return HttpResponse("Page not found", status=404)
+
+def search(request):
+    q = request.GET.get('q')
+    content = util.get_entry(q)
+    if content:
+        return redirect('entry', title=q)
+    else:
+        entries = util.list_entries()
+        filtered_entries = [entry for entry in entries if q.lower() in entry.lower()]
+        logging.info(f"Content for entries '{filtered_entries}'")   
+        return render(request, 'encyclopedia/search.html', {'entries': filtered_entries})        
